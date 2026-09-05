4123dfab3e16314d2d5bb7a21637e7581578152a
kiravalcante <mcavalcante.deb@gmail.com>
2026-09-05 15:47:39 -0300

    add default form data for the search form

---
+++ frontend/src/defaults/search-form/defaults.ts
@@ -0,0 +1,11 @@
+import type { SearchFormData } from "@/types/search-form/types";
+
+export const DEFAULT_SEARCH_FORM_DATA: SearchFormData = {
+  query: "",
+  instruments: {
+    acousticGuitar: true,
+    electricGuitar: true,
+    bass: true,
+  },
+  sortBy: "favorites",
+};
